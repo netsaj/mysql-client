6ab6db9cc9eb836d02a257a986cc5652c2571531
Fabio Enrique Moreno Rangel <fabiomoreno@outlook.com>
2018-07-17 15:54:16 -0500

    actualizado gitignore

--- index.js
+++ index.js
@@ -1,33 +1,5 @@
 'use strict';
 
 var Manager = require('./src/database/dbManager')
-var config = {
-    db :{
-        host     : 'localhost',
-        user     : 'root',
-        password : 'root',
-        database : 'sigesen'
-    },
-    app: {
-        folder : __dirname
-    }
-}
 
-global.config = config;
-
-
-class Main {
-
-    constructor() {
-        this.run()
-    }
-
-    async run(){
-
-        var db = new  Manager();
-
-        var lista =  await db.table('areas').printSql().set('codigo',12).where('id', '=', 1).update();
-        console.log(lista);
-    }
-}
-module.exports = new Main()
+module.exports =  Manager
